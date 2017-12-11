81d13bf044a410c70f35c21f7da25ef60fc2a21f
xxoligann <xxoligann@inbox.lv>
2017-12-11 18:32:40 +0200

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <HeaderComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HeaderComponent from './components/Header'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HeaderComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <header>
+    <h1>Header</h1>
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'header',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
     }
   },
   created () {
-    console.log('hello world');
+    let array = ['1', '2', '3', '4', '5']
+    let hello = 'Hello Juris'
+    console.log(`${hello} and welcome back`)
+    console.log(...array)
   }
 }
 </script>
